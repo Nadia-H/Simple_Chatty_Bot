e882f032061eeaaad48e1952a1fbb5ea9b4f4881
Nadia-H <62909670+Nadia-H@users.noreply.github.com>
2020-11-07 09:13:02 +0100

    Update SimpleBot.java

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class SimpleBot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String yourName;
+        
         System.out.println("Hello! My name is Aid.");
         System.out.println("I was created in 2018.");
         System.out.println("Please, remind me your name.");
-        yourName = scanner.next();
+      
         // reading a name
-
+         String yourName = scanner.nextLine();
         System.out.println("What a great name you have, "+yourName+"!");
     }
 }
